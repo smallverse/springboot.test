6910e1a61933293254c4b755f66e0e9d0a4c4894
zhaijunfeng <zhaijunfeng-2012@foxmail.com>
2019-02-19 18:28:42 +0800

    m

--- springboot.swagger2/src/main/java/com/springboot/swagger2/TestController.java
+++ springboot.swagger2/src/main/java/com/springboot/swagger2/TestController.java
@@ -1,5 +1,7 @@
 package com.springboot.swagger2;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zhaijunfeng
  */
 @RestController
+@Api(value = "测试", description = "测试 RestController")
 @RequestMapping("/test")
 public class TestController {
-    @RequestMapping(name = "/test", method = {RequestMethod.GET})
+    @ApiOperation(value = "测试", notes = "notes 测试")
+    @RequestMapping(value = "/test", method = {RequestMethod.GET})
     public String test() {
         return "test";
     }
+
+    @ApiOperation(value = "获取用户列表", notes = "notes 获取用户列表")
+    @RequestMapping(value = "/getInfo", method = RequestMethod.POST)
+    public String getInfo(String str) {
+        return "getInfo";
+    }
+
 }
